b4b64337dd5cb35fa8952c294b2bb55bf0023bf0
Kartik Mittal <114020941+Kartik13Mittal@users.noreply.github.com>
2024-05-08 15:38:53 +0530

    Update contentScript.js

--- YoutubeAdblocker/contentScript.js
+++ YoutubeAdblocker/contentScript.js
@@ -1,14 +1,14 @@
 // Function to detect ads and perform actions
 function checkForAdsAndActions() {
-    var adOverlay = document.querySelector('.ytp-ad-overlay-container');
-    var adContainer = document.querySelector('.ytp-ad-player-overlay');
-    var skipButton = document.querySelector('.ytp-ad-skip-button-container button');
+    var adOverlay = document.querySelector('.ytp-ad-player-overlay');
+    var adContainer = document.querySelector('.ytp-ad-player-overlay-layout');
+    var skipButton = document.querySelector('.ytp-ad-skip-button-modern');
 
     if (adOverlay || adContainer) {
         console.log('An ad is playing.');
         var video = document.querySelector('video');
         if (video) {
-            video.playbackRate = 7.0; // Speed up playback to 7x during ads
+            video.playbackRate = 10.0; // Speed up playback to 10x during ads
         }
         if (skipButton && !skipButton.disabled) {
             console.log('Skip button is active. Clicking...');
